0be10a93a6938b182bae753fac1b9ef61460c263
Pestich_Dmitriy <dmitriypestich@gmail.com>
2020-09-17 19:43:20 +0300

    prefinal version

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = function () {
 
     let app;
     let width = 800, height = 500;
-    let colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba]; //массив цветов
+    let colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
     let gravity = 4;
     let sec = 500;
     let AreaContainer, interval, positionX, positionY;
